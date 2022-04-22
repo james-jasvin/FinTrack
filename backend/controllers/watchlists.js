3e2200c4fe8fee1469039aabee6e6225ca9bed5c
Devendra Thakur <devendrasingh.thakur@iiitb.ac.in>
2022-04-23 00:25:43 +0530

    APIs added -> getwatchlist by user, delete-watchlist of user, create-Watchlist

--- backend/controllers/watchlists.js
+++ backend/controllers/watchlists.js
@@ -1,6 +1,19 @@
 const watchlistRouter = require('express').Router()
 const Watchlist = require('../models/watchlist')
 
+watchlistRouter.get('/', async (request, response) => {
+    const userId = request.query.user
+
+    const user = request.user
+    if(!user)
+        return response.status(401).json({ error: "token missing or invalid" })
+
+    //const watchlist = await Watchlist.find({})
+    
+	const watchlist = await Watchlist.find({user: userId})
+    
+	response.json(watchlist)
+})
 
 watchlistRouter.post('/', async (request, response) => {
 	const body = request.body
@@ -9,14 +22,34 @@ watchlistRouter.post('/', async (request, response) => {
     if(!user)
         return response.status(401).json({ error: "token missing or invalid" })
 
-		user: user._id
+    //user: user._id
 	const watchlistObject = new Watchlist({
 		name: body.name,
 		isMF: body.isMF,
+        user: user._id
 	})
 
 	const savedWatchlist = await watchlistObject.save()
 	response.status(201).json(savedWatchlist)
 })
 
+watchlistRouter.delete('/:watchlistid', async (request, response) => {
+    const user = request.user    
+    if(!user)
+        return response.status(401).json({ error: "token missing or invalid" })
+
+    const watchlistId = request.params.watchlistid
+    const watchlist = await Watchlist.find({_id: watchlistId})
+
+    if (!watchlist) {
+      return response
+        .status(404)
+        .json({ success: false, msg: `no watchlist with id ${watchlistId}` })
+    }
+
+    await Watchlist.deleteOne({ _id: watchlistId });
+
+    return response.status(201).json({ success: true })
+})
+  
 module.exports = watchlistRouter
