28114c1fe4ccd74d0590ae9623a667f2a2b823c1
jasvin <jasvin.manjaly@gmail.com>
2022-04-25 10:59:09 +0530

    Added better No Watchlist ID UI, added FRONTEND_URL environment variable for watchlist sharing functionality

--- frontend/src/services/watchlists.js
+++ frontend/src/services/watchlists.js
@@ -143,9 +143,6 @@ const deleteWatchlistInstrument = async (watchlistInstrument) => {
   }
 
   const response = await axios.delete(`${watchlistInstrumentUrl}/${watchlistInstrument.id}`, config)
-  // Add request to delete WatchlistInstruments which have watchlistId=${watchlist.id}
-  // Something like, DELETE /api/watchlistInstruments?watchlistId=${watchlist.id}
-  // Use the deleteMany() function in Mongoose to achieve this in the backend
   return response.data
 }
 
@@ -158,10 +155,6 @@ const addWatchlistInstrument = async (watchlistInstrument) => {
   }
 
   const response = await axios.post(watchlistInstrumentUrl, watchlistInstrument, config)
-
-  // Add request to delete WatchlistInstruments which have watchlistId=${watchlist.id}
-  // Something like, DELETE /api/watchlistInstruments?watchlistId=${watchlist.id}
-  // Use the deleteMany() function in Mongoose to achieve this in the backend
   return response.data
 }
 
